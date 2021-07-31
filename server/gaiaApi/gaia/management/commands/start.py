a9b9f8fb7075781c7b35a17d8cc7fd0395254150
canfelli25 <fellitacandini@gmail.com>
2021-07-31 15:44:48 +0700

    Revert "deployment config"
    This reverts commit c886e139b780993b8c754c3066059334161c3cc4.

--- server/gaiaApi/gaia/management/commands/start.py
+++ server/gaiaApi/gaia/management/commands/start.py
@@ -13,5 +13,5 @@ class Command(BaseCommand):
         parser.add_argument('addrport', nargs='?', default='0.0.0.0:' + str(PORT), help='Optional ipaddr:port')
 
     def handle(self, *args, **options):
-        cmd = ['gunicorn', '-b', options['addrport'], 'gaiaApp.wsgi']
+        cmd = ['gunicorn', '-b', options['addrport'], 'pythondjangoapp.wsgi']
         subprocess.call(cmd)
